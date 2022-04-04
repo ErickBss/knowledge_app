9307e3479a2fd1839f4358cf39e60649ce5bf39f
ErickBss <erickbasso22@gmail.com>
2022-04-04 11:10:18 -0300

    login_page

---
+++ src/components/login/index.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router'
+import { FormEvent } from 'react'
+import styles from './style.module.scss'
+
+export function Login() {
+  const router = useRouter()
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault() // disable the feature to reload the page on submit
+
+    router.push('/register')
+  }
+
+  return (
+    <main className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" placeholder="Username" />
+
+        <input type="password" placeholder="Password" />
+        <button
+          type="submit"
+          onClick={(e) => {
+            handleSubmit(e)
+          }}
+        >
+          Login
+        </button>
+      </form>
+    </main>
+  )
+}
